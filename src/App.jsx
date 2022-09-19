561359d824f2f499aa1a980f393928b15ace59f3
enjeytee <enjeytee@gmail.com>
2022-09-19 18:25:26 +0800

    final na talaga

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@
 import React from "react";
 import Menu from "./Menu.jsx";
 import Favorite from "./Favorite.jsx";
+import Toggler from "./Toggler.jsx";
 
 export default function App() {
     return (
         <div>
-            <Menu />
+            <Toggler
+                defaultOnValue={true}
+                render={({on, toggle}) => (
+                    <Menu 
+                        on={on}
+                        toggle={toggle}
+                    />
+                )}
+            />
             <hr/>
             <Favorite />
         </div>
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-import Toggler from "./Toggler.jsx";
 
 function Menu(props) {
     return (
-        <Toggler
-            defaultOnValue={true}
-            render={({on, toggle}) => (
-                <div>
-                    <button onClick={toggle}>{on ? "Hide" : "Show"} Menu</button>
-                    <nav style={{display: on ? "block" : "none"}}>
-                        <h6>Signed in as CoderJohn</h6>
-                        <p><a>Your Profile</a></p>
-                        <p><a>Your Repositories</a></p>
-                        <p><a>Your Stars</a></p>
-                        <p><a>Your Gists</a></p>
-                    </nav>
-                </div>
-            )}
-        />
+        <div>
+            <button onClick={props.toggle}>{props.on ? "Hide" : "Show"} Menu</button>
+            <nav style={{display: props.on ? "block" : "none"}}>
+                <h6>Signed in as CoderJohn</h6>
+                <p><a>Your Profile</a></p>
+                <p><a>Your Repositories</a></p>
+                <p><a>Your Stars</a></p>
+                <p><a>Your Gists</a></p>
+            </nav>
+        </div>
     );
 };
 export default Menu;
